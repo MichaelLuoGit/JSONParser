d38957008ff999299f48199d4cf1588f7e3c1054
michaelluogit <16luom@burlingamehighschool.org>
2014-11-20 23:23:18 -0800

    functional if nothing else

--- CPPTestbed.cpp
+++ CPPTestbed.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <vector>
 using namespace std;
 using namespace rapidjson;
 
@@ -18,12 +19,28 @@ int main() {
 	std::stringstream strStream;
 	strStream << infile.rdbuf();
 	std::string json = strStream.str();
-	std::cout << json;
+	std::cout << json<<endl;
 
+	
 	//Do stuff with json
 	Document d;
 	d.Parse<0>(json.c_str()).HasParseError();
-
+	const Value& colours = d["colorsArray"];
+	assert(colours.IsArray());
+	std::cout << "Colour:\tHex:" <<endl;
+	for (SizeType i = 0; i < colours.Size(); i++)
+	{
+		std::cout << colours[i]["colorName"].GetString();
+		std::cout << "\t";
+		std::cout << colours[i]["hexValue"].GetString()<<endl;
+	}
+	//Use this to store information and eventually print it
+	//std::vector<string> colourInformation;
+	
+	//StringBuffer buffer;
+ //   Writer<StringBuffer> writer(buffer);
+ //   d.Accept(writer);
+	//std::cout << colours[0]["colorName"].GetString();
 	_sleep(10000);
     return 0;
 }
