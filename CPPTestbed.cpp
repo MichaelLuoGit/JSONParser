ecb1177f7ed366c379670e5d831b86950f1c7aa5
michaelluogit <16luom@burlingamehighschool.org>
2014-11-21 23:23:49 -0800

    finished???

--- CPPTestbed.cpp
+++ CPPTestbed.cpp
@@ -10,10 +10,13 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <iomanip>
 using namespace std;
 using namespace rapidjson;
 
 int main() {
+	const int SPACING_COLUMN = 8;
+
 	//Read crap into a file
 	std::ifstream infile("complaintsbyrace.json");
 	std::stringstream strStream;
@@ -28,17 +31,39 @@ int main() {
 	const Value& complaints = d["data"];
 	//assert(complaints.IsArray());
 	//std::cout << "Colour:\tHex:" <<endl;
+	//capture the size of the largest first array
+	float largestGroup = 0;
+	for (SizeType i = 0; i < complaints.Size(); i++)
+	{
+		if (complaints[i][8].GetStringLength() > largestGroup)
+		{
+			largestGroup = complaints[i][8].GetStringLength();
+		}
+	}
+	//Print the useful context for the user
+	std::cout << "Complaints about the police department of San Francisco by race." << endl << endl;
+	std::cout << "Race:" << std::setw(largestGroup - 5 + SPACING_COLUMN) << "2004:" << std::setw(SPACING_COLUMN) << "2005:" << std::setw(SPACING_COLUMN)
+		<< "2006:" << std::setw(SPACING_COLUMN) << "2007:" << std::setw(SPACING_COLUMN) << "2008:" << std::setw(SPACING_COLUMN) << "2009:" << endl << endl;
 	for (SizeType i = 0; i < complaints.Size(); i++)
 	{
 		//We do not want to print anything before the "/n" 
 		bool bUsefulInfo = false;
+		bool bIsGroupName = false;
 		for (SizeType y = 0; y < complaints[i].Size(); y++)
 		{
 			if (bUsefulInfo && complaints[i][y].IsString() == 1 )
 			{
-				cout << complaints[i][y].GetString();
-				cout << "\t";
-				
+				std::cout << complaints[i][y].GetString();
+				if (bIsGroupName)
+				{
+					int hi = largestGroup - complaints[i][y].GetStringLength();
+					std::cout <<std::setw(largestGroup - complaints[i][y].GetStringLength() + SPACING_COLUMN);
+					bIsGroupName = false;
+				}
+				else
+				{
+					std::cout << std::setw(SPACING_COLUMN);
+				}
 			}
 			
 			if (!bUsefulInfo && complaints[i][y].IsString())
@@ -47,11 +72,12 @@ int main() {
 				if (jsonString.compare("{\n}") == 0)
 				{
 					bUsefulInfo = true;
+					bIsGroupName = true;
 				}
 
 			}
 		}
-		cout << "" <<endl;
+		std::cout << "" <<endl;
 		/*
 		std::cout << complaints[i]["colorName"].GetString();
 		std::cout << "\t";
